4c0bb26d7681a74795cf4af404edb95f8c70b783
Tom Hara <tomha@T-HARA.local>
2018-05-04 10:34:39 +0800

    add jquery test

---
+++ custom/js/this.js
@@ -0,0 +1,16 @@
+
+
+var xForm = document.getElementById("theForm");
+// xForm.hidden = true;
+
+
+//TODO: 測試 jquery
+var xForm2 = $("#theForm");
+// xForm2.hide();
+
+var xBtn = $("#xBtn");
+xBtn.click(function(){
+    alert("hello, click");
+});
+
+
