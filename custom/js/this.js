f701d50549a89e7fb73ae19183e47ff57136c17f
Tom Hara <tomha@T-HARA.local>
2018-05-05 09:16:22 +0800

    add: CMD + F2

--- custom/js/this.js
+++ custom/js/this.js
@@ -4,13 +4,24 @@ var xForm = document.getElementById("theForm");
 // xForm.hidden = true;
 
 
+
+$(document).ready(function () {
+    
+});
+
 //TODO: 測試 jquery
 var xForm2 = $("#theForm");
-xForm2.hide(); 
+// xForm2.hide(); 
 
 var xBtn = $("#xBtn");
-xBtn.click(function(){
-    alert("hello, click");
+// xBtn.click(function(){
+//     alert("hello, click");
+// });
+
+
+$("#xBtn").click(function (e) { 
+    e.preventDefault();
+    alert("hello, jq snippet!!");
 });
 
 
