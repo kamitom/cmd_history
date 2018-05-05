7705243628b362000a24a5b495d09c642d3fa895
Tom Hara <tomha@T-HARA.local>
2018-05-05 13:18:54 +0800

    add index2.html

--- custom/js/this.js
+++ custom/js/this.js
@@ -1,27 +1,35 @@
 
 
-var xForm = document.getElementById("theForm");
-// xForm.hidden = true;
-
 
 
 $(document).ready(function () {
-    
+
+    var xForm = document.getElementById("theForm");
+    // xForm.hidden = true;
+
+    var $hideToggle = $("#hideToggle");
+    var $theForm = $(".theForm");
+    $hideToggle.on("click", function () {
+        $theForm.slideToggle(1000);
+    });
+
+    //TODO: 測試 jquery
+    var xForm2 = $("#theForm");
+    // xForm2.hide(); 
+
+    var xBtn = $("#xBtn");
+    // xBtn.click(function(){
+    //     alert("hello, click");
+    // });
+
+
+    $("#xBtn").click(function (e) {
+        e.preventDefault();
+        alert("hello, jq snippet!!");
+    });
+
+
 });
 
-//TODO: 測試 jquery
-var xForm2 = $("#theForm");
-// xForm2.hide(); 
-
-var xBtn = $("#xBtn");
-// xBtn.click(function(){
-//     alert("hello, click");
-// });
-
-
-$("#xBtn").click(function (e) { 
-    e.preventDefault();
-    alert("hello, jq snippet!!");
-});
 
 
