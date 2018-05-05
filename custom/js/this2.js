c94e3601c3e99f300c928c5e43ab878d71077202
Tom Hara <tomha@T-HARA.local>
2018-05-05 17:34:02 +0800

    add: index2.html

--- custom/js/this2.js
+++ custom/js/this2.js
@@ -1,2 +1,23 @@
 
 console.log("hello, new javascript 2!!");
+
+
+let productName3 = "Sleek Fresh Fish";
+let productName4 = "Handmade Plastic Computer";
+let productName5 = "Small Concrete Mouse";
+
+let prodId = "H111";
+let prodId2 = "H222";
+
+let mathRandom = Math.random();
+
+
+console.log(productName5,  prodId2);
+console.log(mathRandom);
+
+
+document.getElementById("txt1").value = Math.random();
+
+document.getElementById("lbl1").innerText = Math.random();
+
+
